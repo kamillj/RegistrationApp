11854cbce96041c3de1f10cedbf83849f1e85ddd
kamillj <kamil.jurczak@gmail.com>
2018-12-15 15:44:59 +0100

    adding command line

---
+++ src/main/java/pl/kamiljurczak/registration/CommandLine.java
@@ -0,0 +1,13 @@
+package pl.kamiljurczak.registration;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandLine implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello");
+    }
+}
